9a39a2992b4097175c85cbc36df8358a6c328344
Scripted Logic <ouassbader184@gmail.com>
2025-02-11 02:25:01 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,6 @@ Route::get('/view', function () {
     return view('acceuil');
 })->name('accueil');
 
-
-Route::get('/test', [App\Http\Controllers\TestController::class, 'we']);
-
 Route::get('/home/{name}', function ($name) {
     return "Bonjour $name";
 });
